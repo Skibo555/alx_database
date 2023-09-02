fc7f788e8e8c92443bb2b7d10b3345ad6bc896cc
Skibo555 <juliusyonwatade@gmail.com>
2023-09-02 20:40:29 +0000

    We create Users

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,18 @@
--- Creating a User and and database.
-
+-- Check if the database exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Check if the user exists
+SELECT user FROM mysql.user WHERE user = 'user_0d_2' LIMIT 1;
 
-GRANT SELECT ON hbtn_0d_2 TO user_0d_2;
+-- If the user does not exist, create the user
+-- If the user exists, update the password
+INSERT INTO mysql.user (Host, User, Password) 
+VALUES ('localhost', 'user_0d_2', PASSWORD('user_0d_2_pwd'))
+ON DUPLICATE KEY UPDATE Password = PASSWORD('user_0d_2_pwd');
 
+-- Grant SELECT privilege to the user on the database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
+
