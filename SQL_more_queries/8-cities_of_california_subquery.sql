aa00d2ffff62127f52e783466b09b3c40583612a
Skibo555 <juliusyonwatade@gmail.com>
2023-09-03 05:06:23 +0000

    We create Users

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,5 @@
--- Use the database passed as an argument
-USE hbtn_0d_usa;
-
 -- Find the state_id for California
 SELECT @california_state_id := id FROM states WHERE name = 'California';
 
 -- List all cities of California using a subquery
 SELECT * FROM cities WHERE state_id = @california_state_id ORDER BY id ASC;
-
